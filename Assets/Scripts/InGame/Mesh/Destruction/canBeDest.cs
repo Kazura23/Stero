e2595fa99a3ea33ece06a6458b1971f8a066eb6b
Alexandre <Alexndre.re@gmail.com>
2017-11-14 15:41:05 +0100

    mergeTool

---
+++ Assets/Scripts/InGame/Mesh/Destruction/canBeDest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canBeDest : MonoBehaviour 
+{	
+	public float ForcePropulse = 5;
+	public float DelayDestruc = 1;
+
+	void OnCollisionEnter ( Collision collision )
+	{
+		if ( collision.collider.tag == Constants._PlayerTag )
+		{
+			StartCoroutine ( GlobalManager.GameCont.MeshDest.SplitMesh ( gameObject, collision.transform, ForcePropulse, DelayDestruc ) );
+		}
+	}
+}
