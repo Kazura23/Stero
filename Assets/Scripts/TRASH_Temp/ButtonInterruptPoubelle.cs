966bdabb0878f0936e9ef2c24b2787ba1829de87
Unknown <tom.kcz@hotmail.fr>
2017-12-22 14:38:32 +0100

    Auto stash before merge of "AlexTmp" and "origin/Alex"

---
+++ Assets/Scripts/TRASH_Temp/ButtonInterruptPoubelle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInterruptPoubelle : AbstractInterrupt {
+    
+    public void PushActive()
+    {
+        this.Active();
+    }
+	
+}
---
+++ Assets/Scripts/TRASH_Temp/testUi.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testUi : UiParent
+{
+	#region Variables
+	public override MenuType ThisMenu
+	{
+		get
+		{
+			return MenuType.test;
+		}
+	}
+	#endregion
+
+	#region Mono
+	#endregion
+
+	#region Public Methods
+	public override void OpenThis ( MenuTokenAbstract GetTok = null )
+	{
+		base.OpenThis ( GetTok );
+	}
+
+	public override void CloseThis ( )
+	{
+		base.CloseThis (  );
+	}
+	#endregion
+
+	#region Private Methods
+	protected override void InitializeUi()
+	{
+	}
+	#endregion
+}
