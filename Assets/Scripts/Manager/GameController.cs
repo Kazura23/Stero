5d40e87e3a2675adc95cfbf52d32de429ab9a0d7
Alexandre <alexndre.re@gmail.com>
2018-01-22 22:43:04 +0100

    DONE_PostProcess ini

--- Assets/Scripts/Manager/GameController.cs
+++ Assets/Scripts/Manager/GameController.cs
@@ -9,6 +9,10 @@ using TMPro;
 using Rewired;
 using UnityEngine.UI;
 using UnityEngine.Analytics;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.PostProcessing;
+using UnityEditor;
+using System;
 
 public class GameController : ManagerParent
 {
@@ -50,6 +54,8 @@ public class GameController : ManagerParent
 
 	[HideInInspector]
 	public bool LaunchTuto;
+
+	public PostProcessingProfile postProfile;
 
 	Player inputPlayer;
 	Text textScore;
@@ -279,6 +285,7 @@ public class GameController : ManagerParent
 		iconeSpe = GlobalManager.Ui.SlowMotion;
 		sliderSpe = GlobalManager.Ui.MotionSlider;
 	}
+
 
     public void ActiveGame()
     {
@@ -530,6 +537,44 @@ public class GameController : ManagerParent
 					addNewScore ( getInfS );
 				}
 				break;
+			}
+	
+		}
+	}
+
+	public void SetAllBonus ( )
+	{
+		iconeSpe.enabled = false;
+		iconeSpe.DOFade ( 0, 0.3f );
+
+		sliderSpe.gameObject.SetActive ( false );
+		sliderSpe.GetComponent<CanvasGroup> ( ).DOFade ( 0, .3f );
+
+		Dictionary <string, ItemModif> getMod = AllModifItem;
+		PlayerController currPlayer = Player.GetComponent<PlayerController> ( );
+		List <ItemModif> AllTI = AllTempsItem;
+		ItemModif thisItem;
+		//List<string> getKey = new List<string> ( );
+
+		SpawnerChunck.EndLevel = 1;
+
+		if ( getMod != null )
+		{
+			foreach ( KeyValuePair <string, ItemModif> thisKV in getMod )
+			{
+				thisItem = thisKV.Value;
+
+				setItemToPlayer ( thisItem, currPlayer );
+			}
+		}
+
+		if ( AllTI != null )
+		{
+			while ( AllTI.Count > 0 )
+			{
+				setItemToPlayer ( AllTI [ 0 ], currPlayer );
+
+				AllTI.RemoveAt ( 0 );
 			}
 		}
 	}
@@ -800,6 +845,16 @@ public class GameController : ManagerParent
         }
     }
 
+	/*float currentValue = 0;
+	void updateBloom ( )
+	{
+		BloomModel.Settings thisBloom = postProfile.bloom.settings;
+
+		thisBloom.bloom.intensity = currentValue;
+
+		postProfile.bloom.settings = thisBloom;
+	}*/
+
     protected override void InitializeManager ( )
 	{
 		if ( UseTuto )
@@ -810,9 +865,17 @@ public class GameController : ManagerParent
 		{
 			LaunchTuto = false;
 		}
+
 		Player = GameObject.FindGameObjectWithTag("Player");
 		thisCam = Player.GetComponentInChildren<Camera> ( );
         musicObject = GlobalManager.AudioMa.transform.Find("Music").gameObject;
+
+		var behaviour = thisCam.GetComponent<PostProcessingBehaviour>();
+
+		postProfile = Instantiate(behaviour.profile, transform);
+		behaviour.profile = postProfile;
+
+		//DOTween.To (()=> currentValue, x=> currentValue = x, 1, 5 ).OnStepComplete( updateBloom );
 
         SpawnerChunck = GetComponentInChildren<SpawnChunks> ( );
 		SpawnerChunck.InitChunck ( );
@@ -844,42 +907,8 @@ public class GameController : ManagerParent
 		} 
 	}
 
-	public void SetAllBonus ( )
-	{
-		iconeSpe.enabled = false;
-		iconeSpe.DOFade ( 0, 0.3f );
-
-		sliderSpe.gameObject.SetActive ( false );
-		sliderSpe.GetComponent<CanvasGroup> ( ).DOFade ( 0, .3f );
-
-		Dictionary <string, ItemModif> getMod = AllModifItem;
-		PlayerController currPlayer = Player.GetComponent<PlayerController> ( );
-		List <ItemModif> AllTI = AllTempsItem;
-		ItemModif thisItem;
-		//List<string> getKey = new List<string> ( );
-
-		SpawnerChunck.EndLevel = 1;
-
-		if ( getMod != null )
-		{
-			foreach ( KeyValuePair <string, ItemModif> thisKV in getMod )
-			{
-				thisItem = thisKV.Value;
-
-				setItemToPlayer ( thisItem, currPlayer );
-			}
-		}
-
-		if ( AllTI != null )
-		{
-			while ( AllTI.Count > 0 )
-			{
-				setItemToPlayer ( AllTI [ 0 ], currPlayer );
-
-				AllTI.RemoveAt ( 0 );
-			}
-		}
-	}
+	void test ()
+	{}
 
 	void setItemToPlayer ( ItemModif thisItem, PlayerController currPlayer )
 	{
