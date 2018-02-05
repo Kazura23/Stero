0b009db0c8b7f7c7599f022b52ae2504a911dec5
Alexandre <alexndre.re@gmail.com>
2018-02-05 14:11:48 +0100

    DONE_HUB

---
+++ Assets/Scripts/InGame/Environment/DeactivateHub.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateHub : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == Constants._PlayerTag ) 
+		{
+			GlobalManager.GameCont.Hub.SetActive ( false );
+		}
+	}
+}
