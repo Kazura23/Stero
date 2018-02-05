b3c82d2bb4ea4e686d9555f9d881ff49e834d35f
Unknown <simon.delavenne@hotmail.fr>
2018-02-05 15:49:07 +0100

    DONE_Heart integration

---
+++ Assets/Level_Design/Heart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			// On gagne un coeur si on n'est pas déjà au max
+
+			Destroy (this.gameObject);
+		}
+	}
+}
