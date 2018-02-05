6cd2b179e04e925397e8f10779b20a29e9df2eea
hugo kostic <h.kostic@rubika-edu.com>
2018-02-05 18:30:20 +0100

    done_somecorretion

---
+++ Assets/Wallctrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallctrl : MonoBehaviour {
+    public GameObject Wall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerExit(Collider other)
+    {
+        Wall.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Wall.SetActive(false);
+    }
+}
