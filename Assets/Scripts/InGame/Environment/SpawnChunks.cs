13e1f5f9cfbb624bfa85809a466cbfca6ae50361
Alexandre <alexndre.re@gmail.com>
2018-02-16 10:50:11 +0100

    DONE_ReplayLevel

--- Assets/Scripts/InGame/Environment/SpawnChunks.cs
+++ Assets/Scripts/InGame/Environment/SpawnChunks.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 public class SpawnChunks : MonoBehaviour 
 {
 	#region Variable
-	public List<ChunksScriptable> ChunksInfo;
-	public List<GameObject> ChunkOnScene;
+	public ChunksScriptable[] ChunksInfo;
+	public LevelReplay[] AllLevelReplay;
+	public GameObject[] ChunkOnScene;
+	
 	public Vector3 DefaultPos;
 
 	[HideInInspector]
@@ -31,9 +33,11 @@ public class SpawnChunks : MonoBehaviour
 	int currChunk = 0;
 	int CurrRandLvl = 0;
 	int saveLvlForStart = 0;
+	int currLevelReplay = 0;
+	int minLevel = 0;
+	
 	bool randAllChunk = false;
 	bool transitChunk = false;
-	int minLevel = 0;
 	#endregion
 	
 	#region Mono
@@ -63,9 +67,9 @@ public class SpawnChunks : MonoBehaviour
 		getSpawnChunks = new List<CheckOnScene> ( );
 		otherSpawn = new List<CheckOnScene> ( );
 		thisT = transform;
-		GetSceneChunk = ChunkOnScene.ToArray ( );
+		GetSceneChunk = ChunkOnScene;
 
-		List<ChunksScriptable> getChunks = ChunksInfo;
+		ChunksScriptable[] getChunks = ChunksInfo;
 		List<List<GetSpawnable>> getSpawnable = new List<List<GetSpawnable>> ( );
 		List<ChunkCombineSpawnble> chunkOrder = new List<ChunkCombineSpawnble> ( );
 		Transform[] getChildrenChunk;
@@ -75,7 +79,7 @@ public class SpawnChunks : MonoBehaviour
 		int c;
 		int currChunkLvl;
 
-		for ( a = 0; a < getChunks.Count; a++ )
+		for ( a = 0; a < getChunks.Length; a++ )
 		{
 			getSpawnable.Add ( new List<GetSpawnable> ( ) );
 
@@ -225,6 +229,7 @@ public class SpawnChunks : MonoBehaviour
 	// Premier chunk à spawn
 	public void FirstSpawn ( )
 	{
+		currLevelReplay = 0;
 		randAllChunk = false;
 		currNbrCh = 0;
 		currLevel = 0;
@@ -317,6 +322,15 @@ public class SpawnChunks : MonoBehaviour
 	// vérification de si il y a un niveau supérieur disponible ou non (si non alors random)
 	void newLevel ( )
 	{
+		for (int a = 0; a < AllLevelReplay.Length; a++)
+		{
+			if ( AllLevelReplay[a].ThisLevel == currLevel && AllLevelReplay[a].NbrReplay > currLevelReplay )
+			{
+				currLevelReplay ++;
+				return;
+			}
+		}
+
 		ChunksScriptable getCunk = thisChunk;
 		List<CheckOnScene> getSpc = getSpawnChunks;
 		Transform getChunkT = getSpc [ getSpc.Count - 1 ].ThisChunk.transform;
@@ -991,4 +1005,11 @@ public class CheckOnScene
 	public List<GameObject> GarbChunk;
 	public GameObject ThisChunk;
 	public bool OnScene;
+}
+
+[System.Serializable]
+public class LevelReplay
+{
+	public int ThisLevel = 0;
+	public int NbrReplay = 0;
 }
